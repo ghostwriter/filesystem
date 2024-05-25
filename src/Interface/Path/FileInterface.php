088a4b913a222de947a8e2c11364f18b46617e18
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-25 17:22:52 -0500

    Add FileInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Path/FileInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Interface\Path;
+
+use Ghostwriter\Filesystem\Interface\PathInterface;
+
+interface FileInterface extends PathInterface {}
