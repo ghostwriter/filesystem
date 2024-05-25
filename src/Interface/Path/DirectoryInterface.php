fb7cb3faed5d30d0a041876417cc29b2f8562d05
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-25 17:22:52 -0500

    Add DirectoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Path/DirectoryInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Interface\Path;
+
+use Generator;
+use Ghostwriter\Filesystem\Interface\PathInterface;
+use IteratorAggregate;
+
+interface DirectoryInterface extends PathInterface
+{
+    //    public function create(): void;
+    //    public function createDirectory(string $name): void;
+    //    public function createFile(string $name, string $contents = ''): void;
+    //    public function createLink(string $name, string $target): void;
+    //    public function createTemporaryDirectory(string $prefix): string;
+    //    public function createTemporaryFile(string $prefix): string;
+    //    public function delete(): void;
+    //    public function deleteRecursive(): void;
+    //    public function directory(string $name): self;
+    //    public function file(string $name): FileInterface;
+    //    public function files(): Generator;
+    //    public function findIn(): Generator;
+    //    public function link(string $name): LinkInterface;
+    //    public function name(): string;
+    //    public function parent(): self;
+    //    public function path(string $name): PathInterface;
+    //    public function realpath(): string;
+    //    public function relativePath(): string;
+    //    public function root(): self;
+    //    public function subdirectories(): Generator;
+    //    public function toString(): string;
+}
