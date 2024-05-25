3faa3222025d9ba018eddeae292c79b0ab8f239f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-05-25 17:23:06 -0500

    Add InvalidLinkPathException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/InvalidLinkPathException.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+use InvalidArgumentException;
+
+final class InvalidLinkPathException extends InvalidArgumentException implements FilesystemExceptionInterface {}
