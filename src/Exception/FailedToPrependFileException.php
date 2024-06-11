43c252c42245429cf80cbffc7af568f1b2a5b6af
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:18 -0500

    Update FailedToPrependFileException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToPrependFileException.php
+++ src/Exception/FailedToPrependFileException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToPrependFileException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToPrependFileException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
