6f6ac614e4ff2214c2922f7644b3ba8c625e33c3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:19 -0500

    Update FailedToCopyFileException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToCopyFileException.php
+++ src/Exception/FailedToCopyFileException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToCopyFileException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToCopyFileException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
