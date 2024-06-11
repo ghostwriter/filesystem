1ff91417a4b3c2754281516734324c274092aefa
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:19 -0500

    Update FailedToCreateLinkException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToCreateLinkException.php
+++ src/Exception/FailedToCreateLinkException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToCreateLinkException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToCreateLinkException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
