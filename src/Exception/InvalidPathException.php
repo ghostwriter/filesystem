a8d1d0ba829f4b9794df155ca99e34ea630de7ad
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:18 -0500

    Update InvalidPathException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/InvalidPathException.php
+++ src/Exception/InvalidPathException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use InvalidArgumentException;
 
-final class InvalidPathException extends InvalidArgumentException implements FilesystemExceptionInterface {}
+final class InvalidPathException extends InvalidArgumentException implements FilesystemExceptionInterface
+{
+}
