874d89a2db9d909c941cf2593ccb783b46b0664b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:18 -0500

    Update FailedToDetermineLinkInfoException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDetermineLinkInfoException.php
+++ src/Exception/FailedToDetermineLinkInfoException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToDetermineLinkInfoException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToDetermineLinkInfoException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
