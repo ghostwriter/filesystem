df41fabe7c11aa3abc7d1df89a9f50cfc2936478
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:19 -0500

    Update FailedToAppendFileException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToAppendFileException.php
+++ src/Exception/FailedToAppendFileException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToAppendFileException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToAppendFileException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
