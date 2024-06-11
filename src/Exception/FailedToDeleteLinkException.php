104ba11e9c4218b92d82f987667da9b097cdb02d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:19 -0500

    Update FailedToDeleteLinkException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDeleteLinkException.php
+++ src/Exception/FailedToDeleteLinkException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToDeleteLinkException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToDeleteLinkException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
