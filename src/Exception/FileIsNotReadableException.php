0e2de5e5f5527a5dbe5538b2437978d64124aa90
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:18 -0500

    Update FileIsNotReadableException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FileIsNotReadableException.php
+++ src/Exception/FileIsNotReadableException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use InvalidArgumentException;
 
-final class FileIsNotReadableException extends InvalidArgumentException implements FilesystemExceptionInterface {}
+final class FileIsNotReadableException extends InvalidArgumentException implements FilesystemExceptionInterface
+{
+}
