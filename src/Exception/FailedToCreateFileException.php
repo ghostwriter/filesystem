7ea9f8194afd6846a4a4e6ddc77e9fe5d0b9adb7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 10:04:19 -0500

    Update FailedToCreateFileException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToCreateFileException.php
+++ src/Exception/FailedToCreateFileException.php
@@ -7,4 +7,6 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToCreateFileException extends RuntimeException implements FilesystemExceptionInterface {}
+final class FailedToCreateFileException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
