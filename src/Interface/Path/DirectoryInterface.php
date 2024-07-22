58a02e3a3721346c4b48a4d6626c972034802ff9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-22 00:22:39 -0500

    Update src/Interface/Path/DirectoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Path/DirectoryInterface.php
+++ src/Interface/Path/DirectoryInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Filesystem\Interface\Path;
 
-use Generator;
 use Ghostwriter\Filesystem\Interface\PathInterface;
 
 interface DirectoryInterface extends PathInterface
