8e283943bbb3f1c35de299180462414c68069861
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:52 -0500

    Add PathTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PathTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Generator;
+use Ghostwriter\Filesystem\Filesystem;
+use Ghostwriter\Filesystem\Interface\PathInterface;
+use Ghostwriter\Filesystem\Path;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+#[CoversClass(Path::class)]
+#[CoversClass(Filesystem::class)]
+final class PathTest extends AbstractTestCase
+{
+    #[DataProvider('providePathCases')]
+    public function testPath(string $path): void
+    {
+        $instance = Path::new($path);
+
+        self::assertSame($path, $instance->toString());
+
+        self::assertSame($path, $instance->__toString());
+
+        self::assertSame($path, $instance->jsonSerialize());
+
+        self::assertInstanceOf(PathInterface::class, $instance);
+    }
+
+    public static function providePathCases(): Generator
+    {
+        yield from [
+            'current' => ['.'],
+            'parent' => ['..'],
+            'root' => ['/'],
+            'relative' => ['path/to/file'],
+            'absolute' => ['/path/to/file'],
+        ];
+    }
+}
