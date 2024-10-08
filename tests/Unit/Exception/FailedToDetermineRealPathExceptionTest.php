6510f445e9fb3e4d4cbd2a4b89c5e0e866d1e025
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:42 -0500

    Update FailedToDetermineRealPathExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDetermineRealPathExceptionTest.php
+++ tests/Unit/Exception/FailedToDetermineRealPathExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDetermineRealPathException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
+#[CoversClass(Filesystem::class)]
 #[CoversClass(FailedToDetermineRealPathException::class)]
-final class FailedToDetermineRealPathExceptionTest extends TestCase
+final class FailedToDetermineRealPathExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
