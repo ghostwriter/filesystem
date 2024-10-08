f9d5ad37f17600f3b0c955d0980e3a7acf0167ec
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 14:28:12 -0500

    Add DestinationAlreadyExistsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/DestinationAlreadyExistsException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+use InvalidArgumentException;
+
+final class DestinationAlreadyExistsException extends InvalidArgumentException implements FilesystemExceptionInterface
+{
+}
