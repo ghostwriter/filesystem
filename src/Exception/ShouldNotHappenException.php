abfd912f4d66fcc04fb33bab66b61bc5afcef65c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:31 -0500

    Add ShouldNotHappenException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/ShouldNotHappenException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+use LogicException;
+
+final class ShouldNotHappenException extends LogicException implements FilesystemExceptionInterface
+{
+}
