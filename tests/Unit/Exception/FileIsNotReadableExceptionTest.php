afcc04dda5c2e31b5bb603cc752e4a21e63a5758
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:46 -0500

    Update FileIsNotReadableExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FileIsNotReadableExceptionTest.php
+++ tests/Unit/Exception/FileIsNotReadableExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FileIsNotReadableException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FileIsNotReadableException::class)]
-final class FileIsNotReadableExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FileIsNotReadableExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
