5ec7b4fba069eeceab6667b3d74027e9a5e42852
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:41 -0500

    Update FailedToDeleteLinkExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDeleteLinkExceptionTest.php
+++ tests/Unit/Exception/FailedToDeleteLinkExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDeleteLinkException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FailedToDeleteLinkException::class)]
-final class FailedToDeleteLinkExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FailedToDeleteLinkExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
