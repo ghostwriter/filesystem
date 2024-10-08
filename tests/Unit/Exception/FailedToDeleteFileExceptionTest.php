cb591695ba64d93438702227e558e812f528db40
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:41 -0500

    Update FailedToDeleteFileExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDeleteFileExceptionTest.php
+++ tests/Unit/Exception/FailedToDeleteFileExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDeleteFileException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FailedToDeleteFileException::class)]
-final class FailedToDeleteFileExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FailedToDeleteFileExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
