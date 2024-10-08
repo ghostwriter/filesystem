ebd6010de8f34282cbc33f8899ac7a9498ede60a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:30 -0500

    Add ErrorException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/ErrorException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+
+final class ErrorException extends \ErrorException implements FilesystemExceptionInterface
+{
+}
