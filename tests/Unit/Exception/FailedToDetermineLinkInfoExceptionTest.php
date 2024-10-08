9f1b529672ba8ed922bbbf9be6e0cfb4ce0924ea
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:42 -0500

    Update FailedToDetermineLinkInfoExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDetermineLinkInfoExceptionTest.php
+++ tests/Unit/Exception/FailedToDetermineLinkInfoExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDetermineLinkInfoException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FailedToDetermineLinkInfoException::class)]
-final class FailedToDetermineLinkInfoExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FailedToDetermineLinkInfoExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
