5f0494234181129af87712d575a3aa33a9b9007f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:47 -0500

    Update InvalidDirectoryPathExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/InvalidDirectoryPathExceptionTest.php
+++ tests/Unit/Exception/InvalidDirectoryPathExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\InvalidDirectoryPathException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(InvalidDirectoryPathException::class)]
-final class InvalidDirectoryPathExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class InvalidDirectoryPathExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
