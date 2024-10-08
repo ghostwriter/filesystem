961d4b355e9ac5bcaba32655f6bdf630f65ba158
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:34 -0500

    Update PathInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/PathInterface.php
+++ src/Interface/PathInterface.php
@@ -4,48 +4,13 @@ declare(strict_types=1);
 
 namespace Ghostwriter\Filesystem\Interface;
 
-use IteratorAggregate;
+use JsonSerializable;
+use Stringable;
 
-/**
- * @extends IteratorAggregate<PathInterface>
- */
-interface PathInterface extends IteratorAggregate
+interface PathInterface extends JsonSerializable, Stringable
 {
-    //    public function absolutePath(): string;
-    //    public function relativePath(): string;
-    public function basename(string $suffix = ''): string;
-
-    public function exists(): bool;
-
-    public function extension(): string;
-
-    public function filename(): string;
-
-    public function isDirectory(): bool;
-
-    public function isExecutable(): bool;
-
-    public function isFile(): bool;
-
-    public function isLink(): bool;
-
-    public function isReadable(): bool;
-
-    public function isWritable(): bool;
-
-    public function lastAccessTime(): int;
-
-    public function lastChangeTime(): int;
-
-    public function lastModifiedTime(): int;
-
-    public function linkTarget(): string;
-
-    public function parent(): self;
-
-    public function realpath(): string;
-
-    public function size(): int;
-
+    /**
+     * @return non-empty-string
+     */
     public function toString(): string;
 }
