3338eab0d9323d1c13a5a97abeb14a07193a45f4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 13:08:53 -0500

    Add FailedToChangeDirectoryException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/FailedToChangeDirectoryException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+use RuntimeException;
+
+final class FailedToChangeDirectoryException extends RuntimeException implements FilesystemExceptionInterface
+{
+}
