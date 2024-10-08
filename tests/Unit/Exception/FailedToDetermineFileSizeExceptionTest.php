72dab5b0fdec2b1de83d493f99dd11b6eddd6a20
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:42 -0500

    Update FailedToDetermineFileSizeExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDetermineFileSizeExceptionTest.php
+++ tests/Unit/Exception/FailedToDetermineFileSizeExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDetermineFileSizeException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FailedToDetermineFileSizeException::class)]
-final class FailedToDetermineFileSizeExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FailedToDetermineFileSizeExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
