9fdadb6f9e1df6434e36768074f3c7f8ce509387
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:30 -0500

    Add LinkDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Exception/LinkDoesNotExistException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Filesystem\Exception;
+
+use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
+use InvalidArgumentException;
+
+final class LinkDoesNotExistException extends InvalidArgumentException implements FilesystemExceptionInterface
+{
+}
