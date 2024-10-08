12c748523dc32255e3d9c7dede3e0a2fd1b3ac76
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:48 -0500

    Update InvalidFilePathExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/InvalidFilePathExceptionTest.php
+++ tests/Unit/Exception/InvalidFilePathExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\InvalidFilePathException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(InvalidFilePathException::class)]
-final class InvalidFilePathExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class InvalidFilePathExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
