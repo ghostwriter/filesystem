f114bc55c3166ea29a4c1f1dff83f4dd96b433cf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:40 -0500

    Update FailedToCreateLinkExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToCreateLinkExceptionTest.php
+++ tests/Unit/Exception/FailedToCreateLinkExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToCreateLinkException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
 #[CoversClass(FailedToCreateLinkException::class)]
-final class FailedToCreateLinkExceptionTest extends TestCase
+#[CoversClass(Filesystem::class)]
+final class FailedToCreateLinkExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
