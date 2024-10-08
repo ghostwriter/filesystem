fd3b75d7e1cf4b4ad5103927f24378cbd1032fab
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-08 10:37:41 -0500

    Update FailedToDetermineCurrentWorkingDirectoryExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/Exception/FailedToDetermineCurrentWorkingDirectoryExceptionTest.php
+++ tests/Unit/Exception/FailedToDetermineCurrentWorkingDirectoryExceptionTest.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Tests\Unit\Exception;
 
 use Ghostwriter\Filesystem\Exception\FailedToDetermineCurrentWorkingDirectoryException;
+use Ghostwriter\Filesystem\Filesystem;
 use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\TestCase;
+use Tests\Unit\AbstractTestCase;
 
+#[CoversClass(Filesystem::class)]
 #[CoversClass(FailedToDetermineCurrentWorkingDirectoryException::class)]
-final class FailedToDetermineCurrentWorkingDirectoryExceptionTest extends TestCase
+final class FailedToDetermineCurrentWorkingDirectoryExceptionTest extends AbstractTestCase
 {
     public function testExample(): void
     {
