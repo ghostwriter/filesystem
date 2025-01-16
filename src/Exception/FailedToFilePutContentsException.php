e18d317e2f80aa9f84202cbba5b4aaafc55f832f
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-16 08:52:58 -0600

    Update FailedToFilePutContentsException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToFilePutContentsException.php
+++ src/Exception/FailedToFilePutContentsException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToFilePutContentsException extends RuntimeException implements FilesystemExceptionInterface
-{
-}
+final class FailedToFilePutContentsException extends RuntimeException implements FilesystemExceptionInterface {}
