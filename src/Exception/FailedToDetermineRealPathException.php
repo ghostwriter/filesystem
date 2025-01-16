2436940a0e5e7df5a74c1721a7bf5c1cb9af5083
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-16 08:52:58 -0600

    Update FailedToDetermineRealPathException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDetermineRealPathException.php
+++ src/Exception/FailedToDetermineRealPathException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToDetermineRealPathException extends RuntimeException implements FilesystemExceptionInterface
-{
-}
+final class FailedToDetermineRealPathException extends RuntimeException implements FilesystemExceptionInterface {}
