3b904215df6ee541acc1093f88aae3a1b1483818
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-16 08:53:01 -0600

    Update SourceDoesNotExistException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/SourceDoesNotExistException.php
+++ src/Exception/SourceDoesNotExistException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use InvalidArgumentException;
 
-final class SourceDoesNotExistException extends InvalidArgumentException implements FilesystemExceptionInterface
-{
-}
+final class SourceDoesNotExistException extends InvalidArgumentException implements FilesystemExceptionInterface {}
