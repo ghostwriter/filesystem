09a20ee5b06d0706119980e00f3b9dd4918646c2
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2025-01-16 08:52:57 -0600

    Update FailedToDeleteDirectoryException.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Exception/FailedToDeleteDirectoryException.php
+++ src/Exception/FailedToDeleteDirectoryException.php
@@ -7,6 +7,4 @@ namespace Ghostwriter\Filesystem\Exception;
 use Ghostwriter\Filesystem\Interface\FilesystemExceptionInterface;
 use RuntimeException;
 
-final class FailedToDeleteDirectoryException extends RuntimeException implements FilesystemExceptionInterface
-{
-}
+final class FailedToDeleteDirectoryException extends RuntimeException implements FilesystemExceptionInterface {}
